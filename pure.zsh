8a1703c16eb02efead6cb3e7529322021e9a1ad2
liyj <liyj@nationalchip.com>
2019-06-20 10:54:49 +0800

    修复 root 用户 name 问题

--- pure.zsh
+++ pure.zsh
@@ -466,7 +466,7 @@ prompt_pure_state_setup() {
 	[[ -n $ssh_connection ]] && username='%F{242}%n@%m%f'
 
 	# show username@host if root, with username in white
-	[[ $UID -eq 0 ]] && username='%%F{red}%n%f@%F{071}%m%f:'
+	[[ $UID -eq 0 ]] && username='%F{red}%n%f@%F{071}%m%f:'
 
 	if [[ "$PURE_SHORT_DIR" == true ]]; then
 		dir='%F{blue}$(_fish_collapsed_pwd)%f '
